8cfc4be3c133c4b703e10f18a4b8634ae010dd71
tanveer-developer123 <tanveerlinux4@gmail.com>
2025-08-19 13:28:35 +0500

    Complete a Home page

--- Noblest/src/App.tsx
+++ Noblest/src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
-import Designing from './components/Designing'
-// import Home from './pages/Home'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      {/* <Home /> */}
-      <Designing/>
+      <Home />
     </>
   )
 }
--- Noblest/src/components/Designing.tsx
+++ Noblest/src/components/Designing.tsx
@@ -8,44 +8,137 @@ import img5 from "../../public/Vector 5.png";
 
 const Designing: React.FC = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-white">
-      <div className="relative w-[700px] h-[400px]">
-        {/* 1 - Vertical bar */}
-        <img
-          src={img1}
-          alt="vector 1"
-          className="h-[480px] absolute left-0 top-0 w-auto"
-        />
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="relative w-[700px] h-[400px] fixed">
+          {/* 1 - Vertical bar */}
+          <img
+            src={img1}
+            alt="vector 1"
+            className="h-[450px] absolute left-0 top-0 w-auto"
+          />
 
-        {/* 2 - Diagonal */}
-        <img
-          src={img2}
-          alt="vector 2"
-          className="absolute left-[120px] top-[120px] h-full w-auto"
-        />
+          {/* 2 - Diagonal */}
+          <img
+            src={img2}
+            alt="vector 2"
+            className="absolute left-[100px] top-[100px] h-[350px] w-auto"
+          />
 
-        {/* 3 - Middle rectangle */}
-        <img
-          src={img3}
-          alt="vector 3"
-          className="absolute left-[320px] bottom-10 h-full w-auto"
-        />
+          {/* 3 - Middle rectangle */}
+          <img
+            src={img3}
+            alt="vector 3"
+            className="absolute left-[280px] bottom-20 h-[350px] w-auto"
+          />
 
-        {/* 4 - Top half circle */}
-        <img
-          src={img4}
-          alt="vector 4"
-          className="absolute left-[480px] top-[-40px] h-[250px] w-auto"
-        />
+          {/* 4 - Top half circle */}
+          <img
+            src={img4}
+            alt="vector 4"
+            className="absolute left-[420px] bottom-[180px] h-[250px] w-auto"
+          />
 
-        {/* 5 - Bottom half circle */}
-        <img
-          src={img5}
-          alt="vector 5"
-          className="absolute left-[480px] bottom-[-120px] h-[250px] w-auto"
-        />
-      </div> 
-    </div>
+          {/* 5 - Bottom half circle */}
+          <img
+            src={img5}
+            alt="vector 5"
+            className="absolute left-[420px] top-[210px] h-[250px] w-auto"
+          />
+        </div>
+      </div>
+      {/* Second Part */}
+      <div className="my-[180px] mx-4 md:mx-10">
+        {/* 1 - Left Image | Right Text */}
+        <div className="flex flex-col md:flex-row items-center">
+          <div className="relative w-full md:w-auto overflow-hidden">
+            <img
+              src={img1}
+              alt="Vector 1"
+              className="w-full h-auto md:h-[400px] object-cover"
+            />
+          </div>
+          <div className="w-[400px] mt-6 md:mt-0 md:mr-6 text-center md:text-left">
+            <h2 className="text-2xl md:text-4xl font-serif font-light">PERSONAL DEV</h2>
+            <p className="text-gray-600 mt-2 text-base md:text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic suscipit dolor quae corporis...
+            </p>
+          </div>
+        </div>
+
+        {/* 2 - Right Image | Left Text */}
+        <div className="flex flex-col md:flex-row-reverse items-center">
+          <div className="relative w-full md:w-auto overflow-hidden">
+            <img
+              src={img2}
+              alt="Vector 2"
+              className="w-full h-auto md:h-[400px] object-cover"
+            />
+          </div>
+          <div className="w-[400px] mt-6 md:mt-0 md:mr-6 text-center md:text-right">
+            <h2 className="text-2xl md:text-4xl font-serif font-light">HEALTH</h2>
+            <p className="text-gray-600 mt-2 text-base md:text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic suscipit dolor quae corporis...
+            </p>
+          </div>
+        </div>
+
+        {/* 3 - Left Image | Right Text */}
+        <div className="flex flex-col md:flex-row items-center">
+          <div className="relative w-full md:w-auto overflow-hidden">
+            <img
+              src={img3}
+              alt="Vector 3"
+              className="w-full h-auto md:h-[400px] object-cover"
+            />
+          </div>
+          <div className="w-[400px] mt-6 md:mt-0 md:mr-6 text-center md:text-left">
+            <h2 className="text-2xl md:text-4xl font-serif font-light">SPIRITUALITY</h2>
+            <p className="text-gray-600 mt-2 text-base md:text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic suscipit dolor quae corporis...
+            </p>
+          </div>
+        </div>
+
+        {/* 4 - Right Image | Left Text */}
+        <div className="flex flex-col md:flex-row-reverse items-center">
+          <div className="relative w-full md:w-auto overflow-hidden">
+            <img
+              src={img4}
+              alt="Vector 4"
+              className="w-full h-auto md:h-[400px] object-cover"
+            />
+          </div>
+          <div className="w-[400px] mt-6 md:mt-0 md:mr-6 text-center md:text-right">
+            <h2 className="text-2xl md:text-4xl font-serif font-light">RELATIONSHIP</h2>
+            <p className="text-gray-600 mt-2 text-base md:text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic suscipit dolor quae corporis...
+            </p>
+          </div>
+        </div>
+
+        {/* 5 - Left Image | Right Text */}
+        <div className="flex flex-col md:flex-row items-center">
+          <div className="w-[300px] mt-6 md:mt-0 md:mr-6 text-center md:text-left">
+            <h2 className="text-2xl md:text-4xl font-serif font-light">FINANCE</h2>
+            <p className="text-gray-600 mt-2 text-base md:text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic suscipit dolor quae corporis...
+            </p>
+          </div>
+          <div className="relative w-full md:w-auto overflow-hidden">
+            <img
+              src={img5}
+              alt="Vector 5"
+              className="w-full h-auto md:h-[400px] object-cover"
+            />
+          </div>
+        </div>
+      </div>
+
+
+
+
+    </>
   );
 };
 
